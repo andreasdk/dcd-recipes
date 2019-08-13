2e4814e3b365be30cc96168b8d14e425c420f77e
Andrew <andrew.kennedy27@outlook.com>
2019-08-13 22:10:12 +0100

    account route initial

--- whisk/routes.py
+++ whisk/routes.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect, request, ses
 from whisk import app, mongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from whisk.forms import LoginForm, RegistrationForm
+from bson.objectid import ObjectId
 
 @app.route("/")
 @app.route("/home")
@@ -27,6 +28,7 @@ def register():
                              'pass': hash_pass})
             session['username'] = request.form['username']
             session['logged_in'] = True
+            flash('Your account has been created! You are now able to log in', 'success')
             return redirect(url_for('home'))
 
         flash('Sorry, username already taken. Please try another.')
@@ -37,25 +39,32 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 #User login function
 def login():
-    # Check is already logged in
+    # Check if already logged in
     if 'logged_in' in session:  
         return redirect(url_for('home'))
 
     form = LoginForm()
     if form.validate_on_submit():
         user = mongo.db.user
-        logged_in_user = user.find_one({
+        existing_user = user.find_one({
                                 'username': request.form['username'].title()})
-
-        if logged_in_user:
-            if check_password_hash(logged_in_user['pass'],
+        # checks that hashed password matches user input
+        if existing_user:
+            if check_password_hash(existing_user['pass'],
                                    request.form['password']):
                 session['username'] = request.form['username']
                 session['logged_in'] = True
                 return redirect(url_for('home'))
-            flash('Sorry incorrect password!')
+       
+            #  else if invalid username/password match    
+            flash('Login Unsuccessful. Please check email and password', 'danger')
             return redirect(url_for('login'))
     return render_template('login.html', form=form, title='Login')
+
+@app.route('/account/')
+# User account page after login
+def account(user_id):
+    return render_template('account.html', title='Account')
 
 @app.route('/logout')
 # Ends session and redirects to homepage
