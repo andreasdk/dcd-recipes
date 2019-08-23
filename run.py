ad7db9761f56b453903ce723825e1c9d9046fb54
Andrew <andrew.kennedy27@outlook.com>
2019-08-23 22:44:28 +0100

    config + create_app class imports

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import os
-from whisk import app, mongo
+from whisk import create_app
+from whisk.config import Config
 
+app = create_app(Config)
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP"), port=os.getenv("PORT"))
