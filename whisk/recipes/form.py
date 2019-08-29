24297fc4ea2d421f1b6035f7f5bbfd465d6b4562
Andrew <andrew.kennedy27@outlook.com>
2019-08-29 21:47:41 +0100

    code formatting to make it easier to read

--- whisk/recipes/form.py
+++ whisk/recipes/form.py
@@ -1,24 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, IntegerField, SelectField
+from wtforms import StringField, SubmitField, TextAreaField, \
+    IntegerField, SelectField
 from wtforms.validators import DataRequired, Optional
 
+
 class RecipeForm(FlaskForm):
+
     recipe_name = StringField('Recipe Name',
-                            validators=[DataRequired()])
+                              validators=[DataRequired()])
     description = TextAreaField('Description',
-                            validators=[DataRequired()])
-    meal_type = SelectField(u'Meal Type',
-                            choices=[('Breakfast', 'Breakfast'), ('Lunch', 'Lunch'), ('Dinner', 'Dinner'), ('Snack', 'Snack'), ('Dessert', 'Dessert')], validators=[DataRequired()])
-    diet_type = SelectField(u'Diet Type',
-                            choices=[('Dairy-Free', 'Dairy-Free'), ('Gluten-Free', 'Gluten-Free'), ('Nut-Free', 'Peanut-Free'), ('Vegan', 'Vegan'), ('Vegetarian', 'Vegetarian')], validators=[Optional()])
-    prep_time = IntegerField('Prep Time',
-                            validators=[DataRequired()])
-    time = IntegerField('Cooking Time',
-                            validators=[DataRequired()])
-    image = StringField('Recipe Image',
+                                validators=[DataRequired()])
+    meal_type = SelectField('Meal Type', choices=[(None, ''), ('Breakfast', 'Breakfast'
+                            ), ('Lunch', 'Lunch'), ('Dinner', 'Dinner'
+                            ), ('Snack', 'Snack'), ('Dessert', 'Dessert'
+                            )], validators=[DataRequired()])
+    diet_type = SelectField('Diet Type', choices=[(None, ''), ('Dairy-Free',
+                            'Dairy-Free'), ('Gluten-Free', 'Gluten-Free'
+                            ), ('Peanut-Free', 'Peanut-Free'), ('Vegan'
+                            , 'Vegan'), ('Vegetarian', 'Vegetarian')],
                             validators=[Optional()])
+    prep_time = IntegerField('Prep Time', validators=[DataRequired()])
+    time = IntegerField('Cooking Time', validators=[DataRequired()])
+    image = StringField('Recipe Image', validators=[Optional()])
     ingredient_name = TextAreaField('Ingredient Name',
-                            validators=[DataRequired()])
+                                    validators=[DataRequired()])
     directions = TextAreaField('Directions',
-                            validators=[DataRequired()])
+                               validators=[DataRequired()])
     submit = SubmitField('Submit Recipe')
