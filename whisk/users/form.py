371b75a39a46b86c9de23873e06a55455251adb4
Andrew <andrew.kennedy27@outlook.com>
2019-09-03 21:17:34 +0100

    code beautify to make it easier to read

--- whisk/users/form.py
+++ whisk/users/form.py
@@ -3,24 +3,27 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+
+    username = StringField('Username', validators=[DataRequired(),
+                           Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+
+    username = StringField('Username', validators=[DataRequired(),
+                           Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+            validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 class PasswordForm(FlaskForm):
-    current_password = PasswordField('Current Password', validators=[DataRequired()])
+
+    current_password = PasswordField('Current Password',
+            validators=[DataRequired()])
     new_password = PasswordField('New Password',
-                                     validators=[DataRequired()])
+                                 validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('new_password')])
+            validators=[DataRequired(), EqualTo('new_password')])
     submit = SubmitField('Change Password')
