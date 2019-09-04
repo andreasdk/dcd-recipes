c6e6f1ed8c489c46d9b47696c92089b05bbd88d0
Andrew <andrew.kennedy27@outlook.com>
2019-09-04 22:55:04 +0100

    removed video play function

--- whisk/static/js/whisk.js
+++ whisk/static/js/whisk.js
@@ -1,6 +1,2 @@
 // On search results page, windows scrolls immediately to search results div
 document.getElementById("search-results").scrollIntoView();
-
-$(document).ready() {
-    $('#video').play();
-}
